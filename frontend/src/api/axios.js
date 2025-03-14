a4ecb8e79015b83d9c1076ef41e065391512fa86
agawopetu@gmail.com <agawopetu@gmail.com>
2025-03-14 15:32:38 +0100

    committing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Ensure this matches your backend URL
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://lab-phase-final.onrender.com/api"
+      : "http://localhost:5000/api", // Ensure this matches your backend URL
 });
 
 // ✅ Attach token to every request if available
